453179c9096fa0bbb0231ebeccc580760364f3e7
Prabin <ghimirey.prabin@gmail.com>
2021-01-27 11:30:12 +0545

    created new app

--- home/views.py
+++ home/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render,HttpResponse
 from home.models import Contact
+from datetime import datetime
 from django.db import models
+
 from django.contrib import messages
 
 # Create your views here.
@@ -19,6 +21,7 @@ def contact(request):
         email=request.POST.get('email')
         phone=request.POST.get('phone')
         contact=Contact(name=name,email=email,phone=phone)
+       
         try:
             contact.full_clean()
             print(contact.full_clean())
